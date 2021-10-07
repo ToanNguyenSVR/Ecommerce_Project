a5d6fbcf8120d9d8588daf2a7a9c59e3eaca3bc8
ToanNguyenSVR <toannguyen.svr@gmail.com>
2021-10-08 03:24:17 +0700

    add back_end application

---
+++ back_end/src/main/java/com/nashtech/nashtech_shop/NashtechShopApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.nashtech_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NashtechShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NashtechShopApplication.class, args);
+    }
+
+}
---
+++ back_end/src/test/java/com/nashtech/nashtech_shop/NashtechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.nashtech_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NashtechShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
